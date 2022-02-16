940609c94a7ed8b17d15b0df08b631b84298892f
zhangzy <zhangzhiyi@bonree.com>
2022-02-16 10:53:38 +0800

    feat: simple7-Label应用（try catch）

--- src/main/java/org/learn/asm/simple/Simple7.java
+++ src/main/java/org/learn/asm/simple/Simple7.java
@@ -13,23 +13,26 @@ import static org.objectweb.asm.Opcodes.*;
  * 使用ASM框架生成class文件
  *
  * @author : zhangzy
- * @date : 2022/2/15
+ * @date : 2022/2/16
  */
-public class Simple6 {
+public class Simple7 {
     /**
      * 生成如下类
-     * public class SimpleDemo6 {
+     * public class SimpleDemo7 {
      *     public void test() {
-     *          for (int i = 0; i < 10; i++) {
-     *              System.out.println(i);
-     *          }
+     *          try {
+     *             System.out.println("start...");
+     *             Thread.sleep(1000);
+     *             System.out.println("end...");
+     *         }catch (InterruptedException e) {
+     *             e.printStackTrace();
      *         }
      *     }
      * }
      */
     private static byte[] generateClazzByte() {
         ClassWriter cw = new ClassWriter(ClassWriter.COMPUTE_FRAMES);
-        cw.visit(V1_8, ACC_PUBLIC, "org/learn/asm/simple/SimpleDemo6", null, "java/lang/Object", null);
+        cw.visit(V1_8, ACC_PUBLIC, "org/learn/asm/simple/SimpleDemo7", null, "java/lang/Object", null);
         //设置构造方法 <init>
         {
             MethodVisitor mv = cw.visitMethod(ACC_PUBLIC, "<init>", "()V", null, null);
@@ -45,22 +48,31 @@ public class Simple6 {
         //设置test方法
         {
             MethodVisitor mv = cw.visitMethod(ACC_PUBLIC, "test", "()V", null, null);
-            Label conditionLabel = new Label();
+            Label startLabel = new Label();
+            Label endLabel = new Label();
+            Label handlerLabel = new Label();
             Label returnLabel = new Label();
 
             mv.visitCode();
-            mv.visitInsn(ICONST_0);
-            mv.visitVarInsn(ISTORE, 1);
+            mv.visitTryCatchBlock(startLabel, endLabel, handlerLabel, "java/lang/InterruptedException");
 
-            mv.visitLabel(conditionLabel);
-            mv.visitVarInsn(ILOAD, 1);
-            mv.visitIntInsn(BIPUSH, 10);
-            mv.visitJumpInsn(IF_ICMPGE, returnLabel);
+            mv.visitLabel(startLabel);
             mv.visitFieldInsn(GETSTATIC, "java/lang/System", "out", "Ljava/io/PrintStream;");
-            mv.visitVarInsn(ILOAD, 1);
-            mv.visitMethodInsn(INVOKEVIRTUAL, "java/io/PrintStream", "println", "(I)V", false);
-            mv.visitIincInsn(1, 1);
-            mv.visitJumpInsn(GOTO, conditionLabel);
+            mv.visitLdcInsn("start...");
+            mv.visitMethodInsn(INVOKEVIRTUAL, "java/io/PrintStream", "println", "(Ljava/lang/String;)V", false);
+            mv.visitLdcInsn(1000L);
+            mv.visitMethodInsn(INVOKESTATIC, "java/lang/Thread", "sleep", "(J)V", false);
+            mv.visitFieldInsn(GETSTATIC, "java/lang/System", "out", "Ljava/io/PrintStream;");
+            mv.visitLdcInsn("end...");
+            mv.visitMethodInsn(INVOKEVIRTUAL, "java/io/PrintStream", "println", "(Ljava/lang/String;)V", false);
+
+            mv.visitLabel(endLabel);
+            mv.visitJumpInsn(GOTO, returnLabel);
+
+            mv.visitLabel(handlerLabel);
+            mv.visitVarInsn(ASTORE, 1);
+            mv.visitVarInsn(ALOAD, 1);
+            mv.visitMethodInsn(INVOKEVIRTUAL, "java/lang/InterruptedException", "printStackTrace", "()V", false);
 
             mv.visitLabel(returnLabel);
             mv.visitInsn(RETURN);
@@ -74,10 +86,10 @@ public class Simple6 {
 
     public static void main(String[] args) throws Exception {
         byte[] bytes = generateClazzByte();
-        String path = FileUtil.getAbsolutePath("/org/learn/asm/simple/SimpleDemo6.class");
+        String path = FileUtil.getAbsolutePath("/org/learn/asm/simple/SimpleDemo7.class");
         FileUtil.writeByteToFile(bytes, path);
 
-        Class<?> clazz = Class.forName("org.learn.asm.simple.SimpleDemo6");
+        Class<?> clazz = Class.forName("org.learn.asm.simple.SimpleDemo7");
         System.out.println(clazz);
         Object obj = clazz.getDeclaredConstructor().newInstance();
         Method test = clazz.getDeclaredMethod("test");
